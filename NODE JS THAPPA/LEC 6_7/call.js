dfeced2d0b0100aaac1b48d0ffed1d40864076bd
HACKERSUMIT404 <mehackathons@gmail.com>
2025-09-08 13:18:55 +0530

    Add files via upload

---
+++ NODE JS THAPPA/LEC 6_7/call.js
@@ -0,0 +1,5 @@
+const add = require('./math')
+console.log(add(5, 3));
+// console.log(subtract(5, 3));
+console.log(multiply(5, 3));
+console.log(divide(5, 3));
---
+++ NODE JS THAPPA/LEC 6_7/global.js
@@ -0,0 +1,13 @@
+console.log(global)
+console.log(globalThis)
+
+global.console.log('Hello from global.js')
+globalThis.console.log('Hello from global.js')
+
+globalThis.console.log(module)
+console.log(globalThis.module)
+
+// console.log(globalThis.process);
+// React-components
+// Node - Modules
+
